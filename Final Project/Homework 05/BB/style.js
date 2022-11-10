06a983d30770e206b1d1c8a6ca965b308fb0206f
bouquet1 <bukuleta@gmail.com>
2022-11-09 19:10:19 -0800

    HW5 - Final Project

---
+++ Final Project/Homework 05/BB/style.js
@@ -0,0 +1,10 @@
+let bars = document.getElementsByClassName("toggle");
+let menu = document.getElementsByClassName("menu");
+
+// how to add and remove class
+
+// define function
+
+function toggleNAv(){
+    
+}
