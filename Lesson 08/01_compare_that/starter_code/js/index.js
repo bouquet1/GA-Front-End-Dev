6cf44aae6ca79393506610fc5fb87f7f1b3f510f
bouquet1 <bukuleta@gmail.com>
2022-11-11 21:02:13 -0800

    SAOA 4(study all over again)

--- Lesson 08/01_compare_that/starter_code/js/index.js
+++ Lesson 08/01_compare_that/starter_code/js/index.js
@@ -35,3 +35,60 @@ if(isNaN(a) || isNaN(b)){
     //  we're taking the string that variable and show it into the text (as a value between two box says either '> , < , = , N/A)  burda diyoruzki comparison i web de sayfada goster .innerText ile diyoruz.
    };
  
+
+
+
+
+  //  EVENT LISTENER EXAMPLES
+  let inputA = document.getElementById("a");
+  // now I have this variable called inputA. I'm gonna add a function.
+    function hail(){
+    console.log("Hail!");
+  }
+    // console.log("For I have been clicked upon!");
+    //   this function says, whenever I click on inputA I want to have the function hail be run.
+  
+  inputA.addEventListener('click',hail);
+  // So this says, Get me my inputA. Define the function called Hail. When I click on the inputA run function hail. Shortly, I am targeting my id 'a' , put in the variable, and then I add a listener to it.
+
+  let inputC = document.getElementById("comparison");
+    function hail2(){
+    console.log("For I have been clicked upon!");
+    }
+
+  inputC.addEventListener('click',hail2);
+  // function line let the hail or hail 2 esixts. Creating variable inputA or inputC is creating a variable. The line ' inputA.addEventListener('click',hail);' or ' inputC.addEventListener('click',hail2);' brings them together. 
+
+  let garfield = document.getElementById("b");
+   function hail3(){
+    console.log("Bro, you've clicked me!");
+   }
+
+   garfield.addEventListener('click',hail3);
+
+
+
+
+
+
+
+  //  More examples 
+  let comicPets = { // this is an object
+    "garfield":{ // garfield is a property of our variable(comicPets)
+      "type":"cat", // type is property of garfield. below more properties with values
+      "color":"orange",
+      "weight":"Fat Beyond All Belief"
+    },
+    "odie":{
+      "type":"dog",
+      "color":"white with spot",
+      "weight":"healthy"
+    },
+    "slyvester":{
+      "type":"cat"
+    }
+  };
+  
+  console.log(comicPets.garfield.weight);
+  // console log gives us Fat Beyond All Belief
+  
--- Lesson 08/02_dom_reference/script.js
+++ Lesson 08/02_dom_reference/script.js
@@ -9,16 +9,20 @@
 // querySelector
 document.querySelector('.my-class-name'); // returns first instance of a class, needs dot before class name
 document.querySelector('#id-of-element'); // returns first instance of a class, needs hash before id name
-document.querySelector('div.good'); // gets div with class of 'good'
+document.querySelector('div.good'); // gets div with class of 'good' - this one targets all the div's in the html with the class good
+
+document.querySelector('.topBar > nav ul > li.featured a');  // If I want to I can be as detailed as I want. I could say this too.  
+
 
 // querySelectorAll
-document.querySelectorAll('a'); // returns an array of all anchor tags in a document
+document.querySelectorAll('a'); // returns an array of all anchor tags in a document - document.querySelector gives you one thing. document.querySelectorAll gives you an array of things, in this case an array of all anchor tags in the html document.
 
 // getElementById
 document.getElementById('id-of-element-goes-here'); // returns entire DOM node and whatever is included inside
 
 // getElementByClassName
-document.getElementsByClassName('class-name-here'); // returns an array of all dom nodes with that class
+document.getElementsByClassName('class-name-here'); // returns an array of all dom nodes with that class. document.getElementsByClassName > watch out that it says getElements - plural
+
 document.getElementsByClassName('class-name-here')[0]; // returns the first element with the class name 'class-name-here
 // the weird [0] thing says to grab the first thing in the array. Don't worry for now!
 
@@ -31,24 +35,32 @@ document.getElementsByClassName('class-name-here')[0]; // returns the first elem
  */
 
 // basic text and style manipulation
-const ga = document.getElementById('ga');
+const ga = document.getElementById('ga'); // <h1 id="ga"></h1> is in the index.html. We created a variable called ga and defined it with 'ga' id element in our html. See below - then we said I wanna change my text this way ga.textContent = "look at me using JS!"; Since we wrote it in js document and we have id "ga" in our html we added the sentence "look at me using JS!" in our html. You can check with the html link that I added before this codes. 
 ga.textContent = "look at me using JS!"; // applies text to an element
-ga.style.backgroundColor = "red" // applies a style to the page, must access style property, then CSS property written in camelCase
+ga.style.backgroundColor = "red" // applies a style to the page, must access style property, then CSS property written in camelCase (css de background-color diye yaziyoruz ama JS de backgroundColor diye yaziyoruz yani camelCAse olarak cunku hypen kullanmiyoruz JS de). BURDADA bu komutla background rengini red yaptik. 
+
+// TO EDIT YOUR CLASSLIST YOU HAVE 3 METHODS THAT IS SHOWN BELOW add, remove, and toggle.
 ga.classList.add('blue'); // adds the class 'blue' to an element
 ga.classList.remove('time-of-day'); // removes the class 'time-of-day' from an element
 ga.classList.toggle('class-name'); // toggles a class on and off of an element
 
-const newParagraph = document.createElement("p") // creates an HTML element of the given tag type
-newParagraph.innerText = "hi im just a child"
-document.body.appendChild(newParagraph); // adds a child HTML element to the body of the page
+
+
+
+const newParagraph = document.createElement("p") // creates an HTML element of the given tag type. This lets you create a whole html element in JS that son't exits. HTML imizde boyle bir <p> yok. brand new ekledik. Mesela yeni bir paragraf ekledik "hi im just a child" diyen html i acarsan gorursun orda.
+newParagraph.innerText = "hi im just a child" // change the text of the paragraph. yukarda yarattigimiz newParagraph in text i bu.
+document.body.appendChild(newParagraph); // adds a child HTML element to the body of the page. This says add the paragraph(newParagraph we created above) to the body of ther document/of our html.
 const greatDiv = document.createElement('div') // creates a div 
-greatDiv.style.border = "2px solid gold";
-greatDiv.innerHTML = '<p>something great goes here</p>'; // sets the inner content of the div
+greatDiv.style.border = "2px solid gold"; // mesela bunu da html sayfamiza ekledik JS ile. 
+greatDiv.innerHTML = '<p>something great goes here</p>'; // sets the inner content of the div. // mesela bunu da html sayfamiza ekledik JS ile. 
 document.body.appendChild(greatDiv); // adds the div to the end of the body of the page
 const greatChild = document.createElement("p"); // creates a p tag
 greatChild.id = "child-element";
-greatChild.innerText = "I'm inside the greatest div of all"; // sets the text of the p tag
+greatChild.innerText = "I'm inside the greatest div of all"; // sets the text of the p tag. // mesela bunu da html sayfamiza ekledik JS ile. 
 greatDiv.appendChild(greatChild); // adds the p tag to the previously created div
+
+
+
 
 // removeChild()
 const getTheKid = document.getElementById('child-element')
@@ -56,7 +68,7 @@ const getTheKid = document.getElementById('child-element')
 // greatDiv.removeChild(getTheKid);
 
 // Attribute changing
-ga.setAttribute('class', 'great-class-name'); // sets an attribute 'great-class-name' on an element
+ga.setAttribute('class', 'great-class-name'); // sets an attribute 'great-class-name' on an element. Attribute ne mesela src=. alt=, ya da href= are attributes.
 ga.removeAttribute('id', 'what-an-id'); // removes 'what-an-id' id from element
 
 // console
