31a91128f211077a1d96fb100c966431dd79e610
bouquet1 <bukuleta@gmail.com>
2022-11-12 17:57:35 -0800

    SAOA 4

--- Homework 04/starter_code/level-one.js
+++ Homework 04/starter_code/level-one.js
@@ -4,15 +4,17 @@
  */
 let score = 0;
 let ballEl = document.querySelector('.js-ball');
-let scoreEl= document.querySelector('.js-score');
+let scoreEl = document.querySelector('.js-score');
 
 ballEl.addEventListener('click', function(){
-  score+=10;
+    score += 10;  
 
-  if(score = 100){
-    scoreEl.innerText = score;
-    declareWinner();
+if ( score < 100 ) {
+  scoreEl.innerText = score;
+} else {
+  declareWinner();
 }
+});
 
 function declareWinner() {
   document.body.classList.add('game-over');
