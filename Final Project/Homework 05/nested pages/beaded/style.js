15ec9b608370dd79b800fe5a794d8b3bf886e976
bouquet1 <bukuleta@gmail.com>
2022-11-16 16:05:13 -0800

    final presentation

---
+++ Final Project/Homework 05/nested pages/beaded/style.js
@@ -0,0 +1,29 @@
+let bars = document.querySelector(".toggle");
+let menu = document.querySelector(".menu");
+
+// how to add and remove class
+
+// define function
+bars.addEventListener('click', function(ompaa){
+    // stop the link
+    ompaa.preventDefault();
+    // get all the items in the nav
+    const itemArr = document.getElementsByClassName('item');
+
+    // loof through all of them
+    for (let i = 0; i < itemArr.length; i++) {
+        // console.log(i + ". " + itemArr[i]);
+          // toggle the class mobileHide
+        itemArr[i].classList.toggle('mobileHide');
+      }
+
+    // get all the item.buttons
+    const itemArrButton = document.querySelectorAll('.item.button');
+    for (let i = 0; i < itemArrButton.length; i++) {
+        // loop through all of them
+        // toggle the mobileHideSM
+        itemArrButton[i].classList.toggle('mobileHideSM');
+        // remove the mobileHide
+        itemArrButton[i].classList.remove('mobileHide');
+    }  
+})
