0fff877313d2da719fe1aed406c1846df0c855e3
bouquet1 <bukuleta@gmail.com>
2022-11-20 19:01:26 -0800

    Honework 6

--- Homework 06/starter_code/js/trulia.js
+++ Homework 06/starter_code/js/trulia.js
@@ -8,21 +8,33 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
 // Toggle the navigation
 
+document.querySelector('trulia-nav-toggle').addEventListener('click', function(e){
+  e.preventDefault();
+  let NavList = documet.querySelectorAll('.trulia-nav > nav ul');
 
+  for(let n=0;n<navSet.length;n++){
+    NavList[n].classList.toggle('trulia-nav-mobilehide');
+  } 
 
-
+  });
 
 
 // Loop through all the cards
+for (var i = 0; i < truliaCards.length; i++){
+  truliaCards[i].addEventListener('click',function(){
     
   // Add a click listener on each card
       
       // Remove the featured class
 
+      for (var q = 0; q < truliaCards.length; q++){
+        truliaCards[q].classList.remove('trulia-featured-grid-item');
+    } 
 
       // Add the featured class on the one clicked on
-
-
+      this.classList.add('trulia-featured-grid-item');
+  });
+}
 
   });  
 
